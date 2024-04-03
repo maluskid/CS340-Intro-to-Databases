89ce3c5ad29f1c05bf76bc13c8dca9d347b180b1
Dominic Maluski <dominic.maluski@gmail.com>
2024-04-03 16:33:24 -0700

    vpn files removed

--- Activities/Activity-1/anyconnect-linux64-4.10.08029-predeploy-k9/anyconnect-linux64-4.10.08029/vpn/load_tun.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-/sbin/lsmod | grep tun > /dev/null
-if [ $? -ne 0 ]; then
- /sbin/modprobe tun > /dev/null 2> /dev/null
- if [ $? -ne 0 ]; then
-   # check for /dev/net/tun
-   [ -c "/dev/net/tun" ] || echo  Warning: Unable to verify that the tun/tap driver is loaded.  Contact your system administrator for assistance.
- fi
-fi
