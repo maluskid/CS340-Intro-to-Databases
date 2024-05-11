7a977ce4fff3f59a80c60926dab57b1a6f88646c
Dominic Maluski <dominic.maluski@gmail.com>
2024-05-10 20:31:09 -0700

    i think i'm getting it

--- Project/test/vite.config.js
+++ Project/test/vite.config.js
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
+import dotenv from 'dotenv';
 import react from '@vitejs/plugin-react'
+
+dotenv.config();
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    port: parseInt(process.env.VITE_SERVER_PORT, 10) || 6001
+  },
+  preview: {
+    port: parseInt(process.env.VITE_PREVIEW_PORT, 10) || 6002
+  }
+});
